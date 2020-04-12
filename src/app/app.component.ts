04818e899bce3a085c98d6c93560cd1adba74451
Estelle OLAWINSKI <estelle@fr.mpitech.com>
2020-04-12 18:19:58 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'projet-angular-ocr';
+  title = 'projet angular ocr';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'eteint',
+    },
+    {
+      name: 'Frigo',
+      status: 'allume',
+    },
+    {
+      name: 'Ordinateur',
+      status: 'eteint',
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+
+  onAllumer() {
+    console.log('on allume tout!');
+  }
 }
